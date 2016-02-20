dde2db57361cad4d6e700f8d2c79e9c819d68b97
beaver-dev <bladerunner.vs@gmail.com>
2016-02-20 19:06:42 +0100

    Add compatibility with Slim v3

--- BbcodeToolbar.php
+++ BbcodeToolbar.php
@@ -17,19 +17,19 @@ class BbcodeToolbar extends BasePlugin
     public function run()
     {
         // Add language files into javascript footer block
-        $this->feather->hooks->bind('view.alter_data', [$this, 'addLanguage']);
+        Container::get('hooks')->bind('view.alter_data', [$this, 'addLanguage']);
         // Support default actions
-        $this->hooks->bind('controller.post.create', [$this, 'addToolbar']);
-        $this->hooks->bind('controller.post.edit', [$this, 'addToolbar']);
-        $this->hooks->bind('controller.topic.display', [$this, 'addToolbar']);
+        Container::get('hooks')->bind('controller.post.create', [$this, 'addToolbar']);
+        Container::get('hooks')->bind('controller.post.edit', [$this, 'addToolbar']);
+        Container::get('hooks')->bind('controller.topic.display', [$this, 'addToolbar']);
         // Support PMs plugin
-        $this->hooks->bind('conversationsPlugin.send.preview', [$this, 'addToolbar']);
-        $this->hooks->bind('conversationsPlugin.send.display', [$this, 'addToolbar']);
+        Container::get('hooks')->bind('conversationsPlugin.send.preview', [$this, 'addToolbar']);
+        Container::get('hooks')->bind('conversationsPlugin.send.display', [$this, 'addToolbar']);
     }
 
     public function addLanguage($data)
     {
-        load_textdomain('bbcode-toolbar', dirname(__FILE__).'/lang/'.$this->feather->user->language.'/bbeditor.mo');
+        load_textdomain('bbcode-toolbar', dirname(__FILE__).'/lang/'.Container::get('user')->language.'/bbeditor.mo');
         $lang_bbeditor = array(
             'btnBold' => __('btnBold', 'bbcode-toolbar'),
             'btnItalic' => __('btnItalic', 'bbcode-toolbar'),
@@ -54,10 +54,10 @@ class BbcodeToolbar extends BasePlugin
 
     public function addToolbar()
     {
-        $this->feather->template->addAsset('css', 'plugins/bbcode-toolbar/style/bbeditor.css', array('type' => 'text/css', 'rel' => 'stylesheet'));
-        $this->feather->template->addAsset('css', 'plugins/bbcode-toolbar/style/colorPicker.css', array('type' => 'text/css', 'rel' => 'stylesheet'));
-        $this->feather->template->addAsset('js', 'plugins/bbcode-toolbar/style/bbeditor.js', array('type' => 'text/javascript'));
-        $this->feather->template->addAsset('js', 'plugins/bbcode-toolbar/style/colorPicker.js', array('type' => 'text/javascript'));
+        View::addAsset('css', 'plugins/bbcode-toolbar/style/bbeditor.css', array('type' => 'text/css', 'rel' => 'stylesheet'));
+        View::addAsset('css', 'plugins/bbcode-toolbar/style/colorPicker.css', array('type' => 'text/css', 'rel' => 'stylesheet'));
+        View::addAsset('js', 'plugins/bbcode-toolbar/style/bbeditor.js', array('type' => 'text/javascript'));
+        View::addAsset('js', 'plugins/bbcode-toolbar/style/colorPicker.js', array('type' => 'text/javascript'));
         return true;
     }
 
